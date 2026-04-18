5467b212dac86892228ef108d25f71c278194aca
julius-boettger <julius.btg@proton.me>
2026-04-18 19:59:29 +0200

    feat: add more operators

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -7,22 +7,26 @@ pub enum Ast<'src> {
     Var(&'src str),
 
     // operators
+
     Neg(Box<Self>),
+
     And(Box<Self>, Box<Self>),
     Or(Box<Self>, Box<Self>),
-    //Imp(Box<Ast<'src>>, Box<Ast<'src>>),
-    //Xor(Box<Ast<'src>>, Box<Ast<'src>>),
-    //Eq(Box<Ast<'src>>, Box<Ast<'src>>),
-    //Neq(Box<Ast<'src>>, Box<Ast<'src>>),
+    Xor(Box<Ast<'src>>, Box<Ast<'src>>),
+
+    Imp(Box<Ast<'src>>, Box<Ast<'src>>),
+
+    Eq(Box<Ast<'src>>, Box<Ast<'src>>),
+    Neq(Box<Ast<'src>>, Box<Ast<'src>>),
 }
 
 pub fn parse<'src>(input: String) -> Ast<'src> {
-    println!("{:?}", parser().parse("!(!(true))&!(false)"));
+    println!("{:?}", parser().parse("a&b|c->d==e!=f"));
     std::process::exit(0) // DEBUG
 }
 
 fn parser<'src>() -> impl Parser<'src, &'src str, Ast<'src>> {
-    let pad_op = |c| just(c).padded();
+    let pad_char_op = |c: char| just(c).padded();
     recursive(|ast| {
         let atom = text::ident().map(|id| match id {
             "true" => Ast::Const(true),
@@ -32,19 +36,37 @@ fn parser<'src>() -> impl Parser<'src, &'src str, Ast<'src>> {
 
         let nested_atom = choice((
             atom,
-            ast.delimited_by(just('('), just(')')),
+            ast.delimited_by(pad_char_op('('), pad_char_op(')')),
         ));
 
-        let negation = pad_op('!')
+        let negation = pad_char_op('!')
             .repeated()
             .foldr(nested_atom, |_, rhs| Ast::Neg(Box::new(rhs)));
 
-        negation.clone().foldl(
+        let and_or_xor = negation.clone().foldl(
             choice((
-                pad_op('&').to(Ast::And as fn(_, _) -> _),
-                pad_op('|').to(Ast::Or as fn(_, _) -> _),
+                pad_char_op('&').to(Ast::And as fn(_, _) -> _),
+                pad_char_op('|').to(Ast::Or as fn(_, _) -> _),
+                pad_char_op('^').to(Ast::Xor as fn(_, _) -> _),
             ))
             .then(negation)
+            .repeated(),
+            |lhs, (operator, rhs)| operator(Box::new(lhs), Box::new(rhs)),
+        );
+
+        let implication = and_or_xor.clone().foldl(
+            just("->").padded().to(Ast::Imp as fn(_, _) -> _)
+            .then(and_or_xor)
+            .repeated(),
+            |lhs, (operator, rhs)| operator(Box::new(lhs), Box::new(rhs)),
+        );
+
+        implication.clone().foldl(
+            choice((
+                just("==").padded().to(Ast::Eq as fn(_, _) -> _),
+                just("!=").padded().to(Ast::Neq as fn(_, _) -> _),
+            ))
+            .then(implication)
             .repeated(),
             |lhs, (operator, rhs)| operator(Box::new(lhs), Box::new(rhs)),
         )
