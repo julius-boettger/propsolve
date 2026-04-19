969a79516c09a163f938dd843fd6f94b99ddfe15
julius-boettger <julius.btg@proton.me>
2026-04-19 14:43:30 +0200

    feat: also solve negated formula

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use crate::{parser::Ast, output::Output};
+
 mod input;
 mod output;
 mod parser;
@@ -8,4 +10,10 @@ fn main() {
     let ast = parser::parse(&input);
     let result = solver::solve(&ast);
     output::output(&result);
+
+    if let Output::Sat(_) = result {
+        let neg_ast = Ast::Neg(Box::new(ast));
+        let result = solver::solve(&neg_ast);
+        output::output_neg_run(&result);
+    }
 }
--- src/output/mod.rs
+++ src/output/mod.rs
@@ -6,18 +6,37 @@ pub enum Output {
     Unknown,
 }
 
+fn print_assignment(assignment: &[(String, bool)]) {
+    let mut assignment: Vec<_> = assignment.to_vec();
+    assignment.sort_unstable_by(|a, b| a.0.cmp(&b.0));
+    for (identifier, value) in assignment {
+        let value = if value { "1".green() } else { "0".red() };
+        println!("{identifier} {} {value}", ":=".bright_black());
+    }
+}
+
 pub fn output(output: &Output) {
     match output {
         Output::Sat(assignment) => {
-            let mut assignment: Vec<_> = assignment.clone();
-            assignment.sort_unstable_by(|a, b| a.0.cmp(&b.0));
-            println!("{}, e.g. with:", format!("formula is {}", "satisfiable".green()).bold());
-            for (identifier, value) in assignment {
-                let value = if value { "1".green() } else { "0".red() };
-                println!("{identifier} {} {value}", ":=".bright_black());
+            print!("{}", format!("formula is {}", "satisfiable".green()).bold());
+            if !assignment.is_empty() {
+                println!(", e.g. with:\n");
+                print_assignment(assignment);
             }
         },
         Output::Unsat => println!("formula is {}", "unsatisfiable".red()),
         Output::Unknown => println!("could not solve formula (result {}), blame z3 🤷", "unknown".red()),
+    }
+}
+
+pub fn output_neg_run(output: &Output) {
+    println!();
+    match output {
+        Output::Sat(assignment) => {
+            println!("{}, e.g. with:\n", format!("formula is also {}", "unsatisfiable".red()).bold());
+            print_assignment(assignment);
+        },
+        Output::Unsat => println!("{}, meaning it is always satisfied", format!("formula is a {}", "tautology".green()).bold()),
+        Output::Unknown => println!("could not solve negated formula (result {}), blame z3 🤷", "unknown".red()),
     }
 }
