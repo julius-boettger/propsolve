dc2d8b9528825073200e2c543a9d407dd89c3fff
julius-boettger <julius.btg@proton.me>
2026-04-19 14:29:47 +0200

    feat: implement output

--- src/output/mod.rs
+++ src/output/mod.rs
@@ -1,3 +1,5 @@
+use colored::Colorize;
+
 pub enum Output {
     Sat(Vec<(String, bool)>),
     Unsat,
@@ -6,8 +8,16 @@ pub enum Output {
 
 pub fn output(output: &Output) {
     match output {
-        Output::Sat(items) => println!("{items:?}"),
-        Output::Unsat => todo!(),
-        Output::Unknown => todo!(),
+        Output::Sat(assignment) => {
+            let mut assignment: Vec<_> = assignment.clone();
+            assignment.sort_unstable_by(|a, b| a.0.cmp(&b.0));
+            println!("{}, e.g. with:", format!("formula is {}", "satisfiable".green()).bold());
+            for (identifier, value) in assignment {
+                let value = if value { "1".green() } else { "0".red() };
+                println!("{identifier} {} {value}", ":=".bright_black());
+            }
+        },
+        Output::Unsat => println!("formula is {}", "unsatisfiable".red()),
+        Output::Unknown => println!("could not solve formula (result {}), blame z3 🤷", "unknown".red()),
     }
 }
