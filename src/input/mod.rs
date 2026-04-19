8a521f15ebd02d888d866f2112de7a90de4f6bba
julius-boettger <julius.btg@proton.me>
2026-04-19 15:05:24 +0200

    feat: parse cli args

--- src/input/mod.rs
+++ src/input/mod.rs
@@ -1,3 +1,25 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(version, about = "Easily describe and solve propositional formulas")]
+struct Args {
+    /// Path to the source file
+    #[arg(required_unless_present = "expression")]
+    file_path: Option<std::path::PathBuf>,
+
+    /// Directly supply expression instead of reading from file
+    #[arg(short, long, conflicts_with = "file_path")]
+    expression: Option<String>,
+}
+
 pub fn get_input() -> String {
-    String::from("(a&b)|(c^d) -> (d == c) & (a != b);\n\n!a;;true;!false; hello_this_is_my_long_id321")
+    let args = Args::parse();
+
+    if let Some(file_path) = args.file_path {
+        todo!()
+    } else if let Some(expression) = args.expression {
+        expression
+    } else {
+        unreachable!()
+    }
 }
